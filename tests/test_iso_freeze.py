3d4f828fde44765648df58d61cd374964ddbcdc4
sbaack <s.baack@posteo.de>
2022-07-27 16:52:46 +0200

    Mock run_pip, not subprocess

--- tests/test_iso_freeze.py
+++ tests/test_iso_freeze.py
@@ -10,19 +10,19 @@ TEST_TOML: Final[Path] = Path(Path(__file__).parent.resolve(), "test_pyproject.t
 def test_validate_pip_version(mocker):
     """Test whether pip version is validated correctly."""
     mocked_pip_version_output_1 = "pip 22.2 from /funny/path/pip (python 3.9)"
-    mocker.patch("subprocess.check_output", return_value=mocked_pip_version_output_1)
+    mocker.patch("iso_freeze.iso_freeze.run_pip", return_value=mocked_pip_version_output_1)
     assert iso_freeze.validate_pip_version(Path("python3")) == True
     mocked_pip_version_output_2 = "pip 22.1 from /funny/path/pip (python 3.9)"
-    mocker.patch("subprocess.check_output", return_value=mocked_pip_version_output_2)
+    mocker.patch("iso_freeze.iso_freeze.run_pip", return_value=mocked_pip_version_output_2)
     assert iso_freeze.validate_pip_version(Path("python3")) == False
     mocked_pip_version_output_3 = "pip 23.1 from /funny/path/pip (python 3.10)"
-    mocker.patch("subprocess.check_output", return_value=mocked_pip_version_output_3)
+    mocker.patch("iso_freeze.iso_freeze.run_pip", return_value=mocked_pip_version_output_3)
     assert iso_freeze.validate_pip_version(Path("python3")) == True
     mocked_pip_version_output_4 = "pip 20.1.3 from /funny/path/pip (python 3.8)"
-    mocker.patch("subprocess.check_output", return_value=mocked_pip_version_output_4)
+    mocker.patch("iso_freeze.iso_freeze.run_pip", return_value=mocked_pip_version_output_4)
     assert iso_freeze.validate_pip_version(Path("python3")) == False
     mocked_pip_version_output_5 = "pip 34.2.9 from /funny/path/pip (python 3.15)"
-    mocker.patch("subprocess.check_output", return_value=mocked_pip_version_output_5)
+    mocker.patch("iso_freeze.iso_freeze.run_pip", return_value=mocked_pip_version_output_5)
     assert iso_freeze.validate_pip_version(Path("python3")) == True
 
 
